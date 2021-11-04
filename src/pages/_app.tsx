b87b42b8fed14d77b87d990bb882d914a2a49aa9
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-04 21:53:37 +0100

    feat: add Layout to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import Layout from '../components/templates/Layout'
 import type { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <Provider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }

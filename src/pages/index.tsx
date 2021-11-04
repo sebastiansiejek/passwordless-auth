5b8cbf44cbc41b8073546f23997000764d3f7b53
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-04 21:54:39 +0100

    fix: remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-import Loader from 'react-spinners/ClipLoader'
-import routes from '../routes/routes'
 import type { NextPage } from 'next'
-import { signOut, signIn, useSession } from 'next-auth/client'
 import User from '../components/User'
 
 const Home: NextPage = () => {

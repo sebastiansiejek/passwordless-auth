859120b88ed4c1e70fa1aba8b36923001ec57bed
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-04 20:33:26 +0100

    feat: add next-auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'next-auth/client'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <Provider session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp

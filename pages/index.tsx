b07d6d52ef773ef8dc865b697f4f9a8511ea14c8
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-11-04 21:18:23 +0100

    feat: add homepage with sign in, sign out and display logged user info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,50 @@
+import Image from 'next/image'
+import Loader from 'react-spinners/ClipLoader'
+import routes from '../routes/routes'
 import type { NextPage } from 'next'
+import { signOut, signIn, useSession } from 'next-auth/client'
 
 const Home: NextPage = () => {
-  return <div></div>
+  const [session, loading] = useSession()
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (session?.user) {
+    const { user } = session
+
+    return (
+      <div>
+        <p>
+          Hello, {user.name} <a href={`mailto:${user.email}`}>{user.email}</a>
+        </p>
+        {user.image && <Image width={40} height={40} src={user.image} alt='avatar' />}
+        <div>
+          <button
+            onClick={() =>
+              signOut({
+                callbackUrl: routes.home.path,
+                redirect: false,
+              })
+            }
+          >
+            Sign out
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <button
+      onClick={() => {
+        signIn()
+      }}
+    >
+      Sign in
+    </button>
+  )
 }
 
 export default Home
